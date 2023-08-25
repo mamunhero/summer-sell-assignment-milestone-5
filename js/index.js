d38c04745cb6a1827bccb4685f9f60a85125797c
mamunhero <96989895+mamunhero@users.noreply.github.com>
2023-08-25 19:52:20 +0600

    first commit

--- js/index.js
+++ js/index.js
@@ -1,46 +1,66 @@
-let count = 1;
-let total = 0;
-function handleClickCard(card) {
-  const selectedItemContainer = document.getElementById("selected-item")
-  const cardNameElement = card.querySelector(".card-title");
-  const cardName = cardNameElement.innerText;
-  const ol = document.createElement("ol");
-  ol.innerText = cardName;
-  selectedItemContainer.appendChild(ol)
-  const priceElement = card.querySelector("span");
-  const price = priceElement.innerText.split(" ")[0];
-  total = parseInt(total) + parseInt(price);
-  document.getElementById("total-price").innerText = total;
+console.log("hello-world")
+// let total = 0;
+// function handleClickCard(card) {
+//   const selectedItemContainer = document.getElementById("selected-item")
+//   const cardNameElement = card.querySelector(".card-title");
+//   const cardName = cardNameElement.innerText;
+//   const li = document.createElement("li");
+//   li.innerText = cardName;
+//   li.style.listStyle = "decimal"
+//   selectedItemContainer.appendChild(li)
+//   const priceElement = card.querySelector("span");
+//   const price = priceElement.innerText.split(" ")[0];
+//   total = parseInt(total) + parseInt(price);
+//   document.getElementById("total-price").innerText = total;
   // discount
-  const discountElement = document.getElementById("discount");
-  const discountElementValue = discountElement.innerText.split(" ")[0];
-  const discountElementValueString = parseFloat(discountElementValue);
-  const discountAmount = (total * 0.2).toFixed(2);
-  discountElement.innerText = discountAmount;
+  // const discountElement = document.getElementById("discount");
+  // const discountElementValue = discountElement.innerText.split(" ")[0];
+  // const discountElementValueString = parseFloat(discountElementValue);
+  // const discountAmount = (total * 0.2).toFixed(2);
+  // discountElement.innerText = total;
   // total
-  const totalElement = document.getElementById("total");
-  const totalElementValue = totalElement.innerText.split(" ")[0];
-  const totalElementValueString = parseFloat(totalElementValue);
-  const totalAmount = (total - discountAmount).toFixed(2);
-  totalElement.innerText = totalAmount;
+  // const totalElement = document.getElementById("total");
+  // const totalElementValue = totalElement.innerText.split(" ")[0];
+  // const totalElementValueString = parseFloat(totalElementValue);
+  // const totalAmount = (total - discountAmount).toFixed(2);
+  // totalElement.innerText = totalAmount;
  if (total>0) {
   document.getElementById("btn-purchase").removeAttribute("disabled")
  }
- if (total<200) {
-  document.getElementById("discount").innerText = "";
- }
- if (total>200) {
-  document.getElementById("btn-apply").removeAttribute("disabled")
- }
+//  if (total<200) {
+//   document.getElementById("discount").innerText = "";
+//  }
+//  if (total>200) {
+//   document.getElementById("btn-apply").removeAttribute("disabled")
+//  }
  
 }
 
 function handleClickBtnApply() {
-  const codeElement = document.getElementById("btn-promo-code");
-  const codeElementValue = codeElement.value;
-  if (codeElementValue === SELL200 ) {
-    document.getElementById("discount").innerText
+  const codeElement = document.getElementById("btn-apply")
+  console.log(codeElement);
+  // const codeElement = document.getElementById("c");
+  // const discountElement = document.getElementById("b");
+  // console.log(discountElement)
+  // const codeElementValue = codeElement.value; 
+  // console.log(codeElementValue)
+  // if (codeElementValue == "SELL" ) {
+    console.log('clisked')
+  // const totalElement = document.getElementById("total");
+  // const totalElementValue = totalElement.innerText.split(" ")[0];
+  // const totalElementValueString = parseFloat(totalElementValue);
+  // const totalAmount = (total - discountAmount).toFixed(2);
+  // totalElement.innerText = totalAmount;
+  // const discountAmount = (total * 0.2).toFixed(2);
+  // discountElement.innerText = "discountAmount";
   }
+  // const totalElement = document.getElementById("total");
+  // const totalElementValue = totalElement.innerText.split(" ")[0];
+  // const totalElementValueString = parseFloat(totalElementValue);
+  // const totalAmount = (total - discountAmount).toFixed(2);
+  // totalElement.innerText = totalAmount;
+  // const discountAmount = (total * 0.2).toFixed(2);
+  // discountElement.innerText = "discountAmount";
 }
 
 
